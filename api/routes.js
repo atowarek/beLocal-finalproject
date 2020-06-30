1c518f2bbc4a2a0d826e678dd108d103787516a9
Esther Urzanqui <estherurzanqui@Esthers-MBP.home>
2020-06-30 11:13:38 +0200

    setting sequeliezer

--- api/routes.js
+++ api/routes.js
@@ -1,16 +1,9 @@
 const express = require('express')
-
-const db = require('./lib/database')
-
+const models = require('../models')
 const routes = express.Router()
 
 routes.get('/users', (req, res) => {
-    db('select * from users;').then(results => {
-        if(results.error) {
-            res.status(400).send({message: 'There was an error'})
-        }
-        res.send(results.data)
-    })   
+    
 })
 
 module.exports = routes
