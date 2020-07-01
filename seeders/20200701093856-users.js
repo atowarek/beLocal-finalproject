fa3dffab67af9d7a45c3ea8f2d57c1e9a67ccabf
Anna Towarek <atowarek@gmail.com>
2020-07-01 11:56:48 +0200

    chore: update

--- seeders/20200701093856-users.js
+++ seeders/20200701093856-users.js
@@ -1,26 +1,30 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [{
-      userName: "atowarek",
-      mail: "atowarek@gmail.com",
-      phone: "654321111",
-      password: "atowarek"
-    },{
-      userName: "eurzanqui",
-      mail: "estherurzanqui81@gmail.com",
-      phone: "654321987",
-      password: "eurzanqui"
-    },{
-      userName: "mclara",
-      mail: "mariona.clto@gmail.com",
-      phone: "654321986",
-      password: "mclara"
-    }]);
+    return queryInterface.bulkInsert('users', [
+      {
+        userName: 'atowarek',
+        mail: 'atowarek@gmail.com',
+        phone: '654321111',
+        password: 'atowarek',
+      },
+      {
+        userName: 'eurzanqui',
+        mail: 'estherurzanqui81@gmail.com',
+        phone: '654321987',
+        password: 'eurzanqui',
+      },
+      {
+        userName: 'mclara',
+        mail: 'mariona.clto@gmail.com',
+        phone: '654321986',
+        password: 'mclara',
+      },
+    ])
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('wine', null, {});
-  }
-};
+    return queryInterface.bulkDelete('user', null, {})
+  },
+}
