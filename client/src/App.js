02547d75e56e3e3660ed69b71f740db320cfc6e7
Anna Towarek <atowarek@gmail.com>
2020-07-02 13:31:27 +0200

    fix components

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,23 @@
 import React from 'react'
 import './App.css'
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import OurNavbar from './components/navbar'
 import HomePage from './components/home-page'
 import About from './components/about'
-
-import Login from './Components/login'
-
+import Login from './components/login'
 
 class App extends React.Component {
-  
-  
   render() {
     return (
       <div className='App'>
-
         <Router>
           <OurNavbar />
           <Switch>
             <Route exact path='/' children={HomePage}></Route>
             <Route exact path='/about' children={About}></Route>
-          <Route exact path='/login' children={Login}></Route>
-      </Switch>
+            <Route exact path='/login' children={Login}></Route>
+          </Switch>
         </Router>
-        
       </div>
     )
   }
