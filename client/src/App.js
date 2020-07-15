a9d8be4db007b4a1368264c7cf1bc202691c3d06
Anna Towarek <atowarek@gmail.com>
2020-07-15 18:33:08 +0200

    fix: navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import OurNavbar from './components/navbar'
@@ -9,54 +9,39 @@ import Signup from './components/form-signup'
 import Dashboard from './components/dashboard'
 import NewActivity from './components/form-new-activity'
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      sessionToken: '',
-    }
-  }
+const App = () => {
+  const [sessionToken, setSessionToken] = useState(null)
 
-  componentDidMount() {
+  useEffect(() => {
     const token = localStorage.getItem('token')
-    if (token && !this.state.sessionToken) {
-      this.setState({ sessionToken: token })
-    }
-  }
+    setSessionToken(token)
+  }, [])
 
-  setSessionState = token => {
-    localStorage.setItem('token', token)
-    this.setState({ sessionToken: token })
-  }
-
-  logout = () => () => {
-    this.setState({
-      sessionToken: '',
-    })
+  const handleLogout = () => () => {
+    setSessionToken(null)
     localStorage.clear()
   }
 
-  render() {
-    return (
-      <div className='App'>
-        <Router>
-          <OurNavbar clickLogout={this.logout} />
-          <Switch>
-            <Route
-              exact
-              path='/'
-              component={HomePage}
-              addActivity={this.addSelectedActivity}></Route>
-            <Route exact path='/about' component={About}></Route>
-            <Route exact path='/signup' component={Signup}></Route>
-            <Route exact path='/login' component={Login}></Route>
-            <Route exact path='/activity' component={NewActivity}></Route>
-            <Route exact path='/dashboard' component={Dashboard}></Route>
-          </Switch>
-        </Router>
-      </div>
-    )
-  }
+  return (
+    <div className='App'>
+      <Router>
+        <OurNavbar sessionToken={sessionToken} logout={handleLogout} />
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/about' component={About}></Route>
+          <Route exact path='/signup' component={Signup}></Route>
+          <Route exact path='/login' component={Login}></Route>
+          <Route
+            exact
+            path='/activity'
+            component={NewActivity}
+            //sessionToken={sessionToken}
+          ></Route>
+          <Route exact path='/dashboard' component={Dashboard}></Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
