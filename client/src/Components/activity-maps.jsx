8dcef0f01d61a16cde8a18d7dda97a142e468383
Anna Towarek <atowarek@gmail.com>
2020-08-23 14:58:25 +0200

    ci: deployment

--- client/src/Components/activity-maps.jsx
+++ client/src/Components/activity-maps.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
-import axios from 'axios'
 
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
 
@@ -10,27 +9,28 @@ const center = {
 }
 
 const mapStyles = {
-    width: "100%",
-    height: "100%",
-    position: 'relative'
-};
+  width: '100%',
+  height: '100%',
+  position: 'relative',
+}
 
 class ActivityMaps extends React.Component {
- 
   render() {
     return (
-      
-      <Map google={this.props.google} styles={mapStyles} zoom={7} initialCenter={center}>
+      <Map
+        google={this.props.google}
+        styles={mapStyles}
+        zoom={7}
+        initialCenter={center}>
         {this.props.activities.map((activity, index) => (
           //console.log(activity),
           <Marker
             name={activity.name}
-            key={activity.id}
+            key={index}
             position={{ lat: activity.latitude, lng: activity.longitude }}
           />
         ))}
       </Map>
-     
     )
   }
 }
